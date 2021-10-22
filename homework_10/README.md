43af91c7cf8567140af0a5fd5f536f3f16b673dd
barmank32 <barmank@yandex.ru>
2021-10-22 09:57:48 +0300

    homework_10

--- homework_10/README.md
+++ homework_10/README.md
@@ -7,4 +7,39 @@
 Изучаем тип JSON
 
 1. проанализировать типы данных в своем проекте, изменить при необходимости. В README указать что на что поменялось и почему.
+
+Наверно я бы изменил `bigint` во всей таблице на `int unsigned` чтобы убрать оверхед.
+
 2. добавить тип JSON в структуру. Проанализировать какие данные могли бы там хранится. привести примеры SQL для добавления записей и выборки.
+
+``` sql
+CREATE TABLE IF NOT EXISTS Products (
+	products_id bigint NOT NULL,
+	name varchar(50) NOT NULL,
+	description TEXT,
+	price FLOAT NOT NULL,
+    attributes JSON NOT NULL ,
+	CONSTRAINT Products_pk PRIMARY KEY (products_id)
+)
+ENGINE=InnoDB;
+
+INSERT INTO Products (
+    `name` ,
+    `price` ,
+    `attributes`
+)
+VALUES(
+    'Aspirin' ,
+    '50' ,
+    JSON_OBJECT(
+        "form",
+        "blister",
+        "qtl",
+        "14"
+    )
+);
+SELECT * FROM Products
+WHERE
+JSON_EXTRACT(`attributes`, '$.form') LIKE 'blister';
+```
+Данный хранимые в JSON могут быть параметры упаковки и лекарства.
---
+++ homework_10/init.sql
@@ -0,0 +1,90 @@
+CREATE database IF NOT EXISTS apteka;
+
+USE apteka;
+
+CREATE TABLE IF NOT EXISTS Products (
+	products_id INT UNSIGNED NOT NULL,
+	name varchar(50) NOT NULL,
+	description TEXT,
+	price FLOAT NOT NULL,
+	CONSTRAINT Products_pk PRIMARY KEY (products_id)
+)
+ENGINE=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS ProductsCategories (
+	products_id INT UNSIGNED NOT NULL,
+	categories_id INT UNSIGNED NOT NULL
+)
+ENGINE=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS Categories (
+	categories_id INT UNSIGNED NOT NULL,
+	name varchar(50) NOT NULL,
+	description TEXT,
+	CONSTRAINT Categories_pk PRIMARY KEY (categories_id)
+)
+ENGINE=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS OrdersProducts (
+	order_id INT UNSIGNED NOT NULL,
+	products_id INT UNSIGNED NOT NULL,
+	qty int NOT NULL,
+	total_price FLOAT NOT NULL
+)
+ENGINE=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS Orders (
+	order_id INT UNSIGNED NOT NULL,
+	created DATE NOT NULL,
+	payed DATE NOT NULL,
+	price FLOAT NOT NULL,
+	discount FLOAT NOT NULL,
+	customer_id INT UNSIGNED NOT NULL,
+	status varchar(50) NOT NULL,
+	CONSTRAINT Orders_pk PRIMARY KEY (order_id)
+)
+ENGINE=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS Customers (
+	customer_id INT UNSIGNED NOT NULL,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	tel varchar(50) NOT NULL,
+	password varchar(50) NOT null,
+	CONSTRAINT customer_pk PRIMARY KEY (customer_id)
+)
+ENGINE=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS CustomerDiscount (
+	discount FLOAT NOT NULL,
+	customer_id INT UNSIGNED NOT NULL
+)
+ENGINE=InnoDB;
+
+
+
+
+ALTER TABLE ProductsCategories ADD CONSTRAINT ProductsCategories_fk0 FOREIGN KEY (products_id) REFERENCES Products(products_id);
+ALTER TABLE ProductsCategories ADD CONSTRAINT ProductsCategories_fk1 FOREIGN KEY (categories_id) REFERENCES Categories(categories_id);
+
+
+ALTER TABLE OrdersProducts ADD CONSTRAINT OrdersProducts_fk0 FOREIGN KEY (order_id) REFERENCES Orders(order_id);
+ALTER TABLE OrdersProducts ADD CONSTRAINT OrdersProducts_fk1 FOREIGN KEY (products_id) REFERENCES Products(products_id);
+
+ALTER TABLE Orders ADD CONSTRAINT Orders_fk0 FOREIGN KEY (customer_id) REFERENCES Customers(customer_id);
+
+
+ALTER TABLE CustomerDiscount ADD CONSTRAINT CustomerDiscount_fk0 FOREIGN KEY (customer_id) REFERENCES Customers(customer_id);
